Fix off by one error in T&C metrics

coranking/test/metrics_test.py
@@ -1,8 +1,39 @@
 from coranking import coranking_matrix
 from coranking.metrics import trustworthiness, continuity, LCMC
-from nose.tools import *
+from nose import tools as nose
 import numpy as np
+import numpy.testing as npt
 from sklearn import manifold, datasets
+
+
+def test_coranking_matrix_perfect_case():
+    high_data = np.eye(3)
+    low_data = np.eye(3)
+    Q = coranking_matrix(high_data, low_data)
+
+    expected = np.array([[3, 0], [0, 3]])
+    npt.assert_almost_equal(Q, expected)
+
+
+def test_trustworthiness_perfect_case():
+    high_data = np.eye(5)
+    low_data = np.eye(5)
+
+    Q = coranking_matrix(high_data, low_data)
+    t = trustworthiness(Q.astype(np.int64), min_k=2)
+
+    nose.assert_equal(t, 1.)
+
+
+def test_continuity_perfect_case():
+    high_data = np.eye(5)
+    low_data = np.eye(5)
+
+    Q = coranking_matrix(high_data, low_data)
+    c = continuity(Q.astype(np.int64), min_k=2)
+
+    nose.assert_equal(c, 1.)
+
 
 def test_trustworthiness():
     high_data, low_data = make_datasets()
@@ -10,7 +41,7 @@ def test_trustworthiness():
     Q = coranking_matrix(high_data, low_data)
     t = trustworthiness(Q.astype(np.int64), min_k=5, max_k=6)
 
-    assert_almost_equal(t, 0.895, places=3)
+    nose.assert_almost_equal(t, 0.895, places=3)
 
 
 def test_trustworthiness_array():
@@ -19,7 +50,7 @@ def test_trustworthiness_array():
     Q = coranking_matrix(high_data, low_data)
     result = trustworthiness(Q)
 
-    assert_equal(result.shape, (297, ))
+    nose.assert_equal(result.shape, (297, ))
 
 
 def test_continuity():
@@ -28,10 +59,10 @@ def test_continuity():
     Q = coranking_matrix(high_data, low_data)
     c = continuity(Q, 5, 6)
 
-    assert_almost_equal(c, 0.982, places=3)
+    nose.assert_almost_equal(c, 0.982, places=3)
 
     c2 = trustworthiness(Q, 5, 6)
-    assert_true(c, c2)
+    nose.assert_true(c, c2)
 
 
 def test_continuity_array():
@@ -40,7 +71,7 @@ def test_continuity_array():
     Q = coranking_matrix(high_data, low_data)
     result = continuity(Q)
 
-    assert_equal(result.shape, (297, ))
+    nose.assert_equal(result.shape, (297, ))
 
 
 def test_LCMC():
@@ -49,7 +80,7 @@ def test_LCMC():
     Q = coranking_matrix(high_data, low_data)
     l = LCMC(Q, 5, 6)
 
-    assert_almost_equal(l, 0.377, places=3)
+    nose.assert_almost_equal(l, 0.377, places=3)
 
 
 def test_LCMC_array():
@@ -58,7 +89,7 @@ def test_LCMC_array():
     Q = coranking_matrix(high_data, low_data)
     result = LCMC(Q)
 
-    assert_equal(result.shape, (297, ))
+    nose.assert_equal(result.shape, (297, ))
 
 def make_datasets():
     high_data, color \